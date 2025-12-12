5b433123742dc6893402f1fd7c7742ca53c7dc3b
回首向 <40594978+hsx2333@users.noreply.github.com>
2025-12-12 23:00:30 +0800

    Update Weather.vue

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,114 +1,85 @@
 <template>
-  <div class="weather" v-if="weatherData.adCode.city && weatherData.weather.weather">
-    <span>{{ weatherData.adCode.city }}&nbsp;</span>
-    <span>{{ weatherData.weather.weather }}&nbsp;</span>
-    <span>{{ weatherData.weather.temperature }}℃</span>
-    <span class="sm-hidden">
-      &nbsp;{{
-        weatherData.weather.winddirection?.endsWith("风")
-          ? weatherData.weather.winddirection
-          : weatherData.weather.winddirection + "风"
-      }}&nbsp;
-    </span>
-    <span class="sm-hidden">{{ weatherData.weather.windpower }}&nbsp;级</span>
+  <div class="weather" v-if="weather.city && weather.text">
+    <span>{{ weather.city }}&nbsp;</span>
+    <span>{{ weather.text }}&nbsp;</span>
+    <span>{{ weather.temp }}℃</span>
+    <span class="sm-hidden">&nbsp;{{ weather.windDir }}&nbsp;</span>
+    <span class="sm-hidden">{{ weather.windScale }}&nbsp;级</span>
   </div>
+
   <div class="weather" v-else>
     <span>天气数据获取失败</span>
   </div>
 </template>
 
 <script setup>
-import { getAdcode, getWeather, getOtherWeather } from "@/api";
 import { Error } from "@icon-park/vue-next";
 
-// 高德开发者 Key
-const mainKey = import.meta.env.VITE_WEATHER_KEY;
+// MXNZP 配置
+const MX_APP_ID = import.meta.env.VITE_IP_APP_ID;
+const MX_APP_SECRET = import.meta.env.VITE_IP_APP_SECRET;
 
-// 天气数据
-const weatherData = reactive({
-  adCode: {
-    city: null, // 城市
-    adcode: null, // 城市编码
-  },
-  weather: {
-    weather: null, // 天气现象
-    temperature: null, // 实时气温
-    winddirection: null, // 风向描述
-    windpower: null, // 风力级别
-  },
+// 和风天气 key
+const QKEY = import.meta.env.VITE_QWEATHER_KEY;
+
+const weather = reactive({
+  city: null,
+  text: null,
+  temp: null,
+  windDir: null,
+  windScale: null,
 });
 
-// 取出天气平均值
-const getTemperature = (min, max) => {
+const getCityFromIP = async () => {
+  const url = `https://www.mxnzp.com/api/ip/self?app_id=${MX_APP_ID}&app_secret=${MX_APP_SECRET}`;
+  const res = await fetch(url);
+  return res.json();
+};
+
+const getCityId = async (cityName) => {
+  const url = `https://geoapi.qweather.com/v2/city/lookup?location=${cityName}&key=${QKEY}`;
+  const res = await fetch(url);
+  return res.json();
+};
+
+const getWeatherNow = async (locationId) => {
+  const url = `https://devapi.qweather.com/v7/weather/now?location=${locationId}&key=${QKEY}`;
+  const res = await fetch(url);
+  return res.json();
+};
+
+const loadWeather = async () => {
   try {
-    // 计算平均值并四舍五入
-    const average = (Number(min) + Number(max)) / 2;
-    return Math.round(average);
-  } catch (error) {
-    console.error("计算温度出现错误：", error);
-    return "NaN";
+    if (!MX_APP_ID || !MX_APP_SECRET) throw "未配置 MXNZP IP API";
+    if (!QKEY) throw "未配置和风天气 Key";
+
+    // 1. IP 反查城市
+    const ipRes = await getCityFromIP();
+    const cityRaw = ipRes.data.city;        // 比如：深圳市
+    const city = cityRaw.replace(/市/g, ""); // 变成：深圳
+
+    weather.city = city;
+
+    // 2. 用城市名查和风城市 ID
+    const cityRes = await getCityId(city);
+    const locationId = cityRes.location[0].id;
+
+    // 3. 获取实时天气
+    const w = await getWeatherNow(locationId);
+    const now = w.now;
+
+    weather.text = now.text;
+    weather.temp = now.temp;
+    weather.windDir = now.windDir;
+    weather.windScale = now.windScale;
+  } catch (err) {
+    ElMessage({
+      message: "天气数据获取失败：" + err,
+      icon: h(Error, { theme: "filled", fill: "#efefef" }),
+    });
+    console.error(err);
   }
 };
 
-// 获取天气数据
-const getWeatherData = async () => {
-  try {
-    // 获取地理位置信息
-    if (!mainKey) {
-      console.log("未配置，使用备用天气接口");
-      const result = await getOtherWeather();
-      console.log(result);
-      const data = result.result;
-      weatherData.adCode = {
-        city: data.city.City || "未知地区",
-        // adcode: data.city.cityId,
-      };
-      weatherData.weather = {
-        weather: data.condition.day_weather,
-        temperature: getTemperature(data.condition.min_degree, data.condition.max_degree),
-        winddirection: data.condition.day_wind_direction,
-        windpower: data.condition.day_wind_power,
-      };
-    } else {
-      // 获取 Adcode
-      const adCode = await getAdcode(mainKey);
-      console.log(adCode);
-      if (adCode.infocode !== "10000") {
-        throw "地区查询失败";
-      }
-      weatherData.adCode = {
-        city: adCode.city,
-        adcode: adCode.adcode,
-      };
-      // 获取天气信息
-      const result = await getWeather(mainKey, weatherData.adCode.adcode);
-      weatherData.weather = {
-        weather: result.lives[0].weather,
-        temperature: result.lives[0].temperature,
-        winddirection: result.lives[0].winddirection,
-        windpower: result.lives[0].windpower,
-      };
-    }
-  } catch (error) {
-    console.error("天气信息获取失败:" + error);
-    onError("天气信息获取失败");
-  }
-};
-
-// 报错信息
-const onError = (message) => {
-  ElMessage({
-    message,
-    icon: h(Error, {
-      theme: "filled",
-      fill: "#efefef",
-    }),
-  });
-  console.error(message);
-};
-
-onMounted(() => {
-  // 调用获取天气
-  getWeatherData();
-});
+onMounted(loadWeather);
 </script>
