365f6287e75abc5cf25aaa28a55dc43e017987f1
回首向 <40594978+hsx2333@users.noreply.github.com>
2025-12-13 17:35:48 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,18 +14,18 @@
         </span>
         <!-- 以下信息请不要修改哦 -->
         <span class="hidden">
-          &nbsp;Made&nbsp;by
+          &amp;&nbsp;Made&nbsp;by
           <a :href="config.github" target="_blank">
             {{ config.author }}
           </a>
         </span>
         <!-- 站点备案 -->
-        <span>
-          &amp;
-          <a v-if="siteIcp" href="https://beian.miit.gov.cn" target="_blank">
-            {{ siteIcp }}
-          </a>
-        </span>
+      <span v-if="siteIcp">
+        &amp;
+        <a href="https://beian.miit.gov.cn" target="_blank">
+          {{ siteIcp }}
+        </a>
+      </span>
       </div>
       <div v-else class="lrc">
         <Transition name="fade" mode="out-in">
